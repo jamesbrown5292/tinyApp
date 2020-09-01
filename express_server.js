f27006461f7933314f9b3e0c20b0a783628ad4e6
James Brown <jamesbrown5292@gmail.com>
2020-09-01 02:38:27 +0000

    added random string generator

--- express_server.js
+++ express_server.js
@@ -6,6 +6,25 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const generateRandomString = () => {
+  //loop from 1-6
+  //generate a random number between (48 - 90 excluding 58 - 64), get char code from this number
+  let retString = "";
+  while (retString.length < 6) {
+    let raNum = Math.random() * (90 - 49) + 48;
+    if (raNum >= 58 && raNum <= 64) {
+      continue;
+    } else {
+      let addChar = String.fromCharCode(raNum);
+      retString += addChar;
+    }
+  }
+  return retString.toLowerCase();
+};
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 //set the express view engine to ejs otherwise its default will be jade
 app.set('view engine', 'ejs');
 
@@ -30,7 +49,16 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase};
   res.render('urls_index', templateVars);
 });
+//handle data being posted from the new page
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
+//add a route for creating a new tiny url
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 //add a route handler for /urls__show
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}; //question - where is it pulling params from? what does the request object look like?
