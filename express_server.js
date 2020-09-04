ce64fda389f042243c31c6bdbf702da7b0532e86
James Brown <jamesbrown5292@gmail.com>
2020-09-04 03:43:58 +0000

    refactoring and stopped non-owners from viewing urls they don't own

--- express_server.js
+++ express_server.js
@@ -1,67 +1,75 @@
-
+//Import packages for cookies, body parsing, method override, bcrypt, set date email and random string function and set view engine and session cookie
 const cookieSession = require('cookie-session');
 const bodyParser = require("body-parser");
 const express = require("express");
 const methodOverride = require('method-override')
 const bcrypt = require("bcrypt");
-const helpers = require("./helpers")
+const helpers = require("./helpers");
+const date = require('date-and-time');
 const app = express();
 const emailLookup = helpers.emailLookup;
+const generateRandomString = helpers.generateRandomString;
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({name: 'Session', keys: ['user_id']}));
 
 const PORT = 8080; // default port 8080
-//store urls to access
 
+/*Empty url database and users object. Url database structured as follows:
+{
+  "ck6sk3": {
+    longURL: "www.example.com" 
+    userID: sdjd8e, 
+    fav: false, 
+    visitCount: 0, 
+    uniqueVisits: [], 
+    visitTimes: [],
+    dateCreated: 
+  }
+}
+
+Users object structured as follows: 
+
+  {
+    "sjd73k": {
+      id: "sjd73k", 
+      email: "example@email.com,""
+      hashedPassword: aslsaksalasdk83292023i23jhf
+    }
+  }
+
+*/
 const urlDatabase = {};
 const users = {};
 
-const generateRandomString = () => {
-  //loop from 1-6
-  //generate a random number between (48 - 90 excluding 58 - 64), get char code from this number
-  let retString = "";
-  while (retString.length < 6) {
-    let raNum = Math.ceil(Math.random() * (90 - 49) + 48);
-    if (raNum >= 58 && raNum <= 64) {
-      continue;
-    } else {
-      let addChar = String.fromCharCode(raNum);
-      retString += addChar;
-    }
-  }
-  return retString.toLowerCase();
-};
-//set the express view engine to ejs otherwise its default will be jade
+
+//Use middleware to create a visitor id cookie if the request doesnt aready have
 app.use((req, res, next) => {
-  //create a visitor id cookie if the request doesnt aready habe one
   if (!req.session.user_id) {
     req.session.user_id = generateRandomString();
   }
   next();
 });
 
-//handle get requests
+//Redirect requests to "/" to the login page if user is not logged in, or to the urls page if they are
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  const userIDCookie = req.session.user_id;
+  if (!users[userIDCookie]) {
+    res.redirect("/login");
+  } else {
+    res.redirect("/urls")
+  }
 });
+
 //listen on the server
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-//additional endpooint
-app.get("/urls.json", (req, res) => {
-  res.send(urlDatabase);
-});
 
-//send some additional html in the response to a different URI
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-//add a route handler for /urls
+//add a route handler for /urls. Check that the user_id matches the session cookie. If not, redirect to login, otherwise...
+//...pass the original list of urls and an filtered list of URLS to the urls_index ejs. The filtered list will be rendered to the user's index page
 app.get("/urls", (req, res) => {
   const userIDCookie = req.session.user_id;
   const filteredUsers = {};
@@ -71,20 +79,19 @@ app.get("/urls", (req, res) => {
     for (let url in urlDatabase) {
       let userID = urlDatabase[url]['userID'];
       if (userID === userIDCookie ) {
-        //can't delete the database or websites not saved between sessions 
         filteredUsers[url] = url
       }
     }
-    //urls needs to be filtered here to only include ones wtith user_id
     const templateVars = { urls: filteredUsers, user: users[userIDCookie], unfilteredURL: urlDatabase };
     res.render('urls_index', templateVars);
   }
 });
 
-//handle new short URL being posted from the new page and redirect user to new page
+//handle new short URL being created in the urls_new page
 app.post("/urls", (req, res) => {
-  const randURL = generateRandomString();
-  const shortURL = randURL
+  const creationDate = date.format(new Date(), 'MMM D YYYY hh:mm [UTC]', true)
+  const randomURL = generateRandomString();
+  const shortURL = randomURL
   const longURL = req.body.longURL;
   const userID = req.session.user_id
   urlDatabase[shortURL] = {longURL, 
@@ -92,13 +99,14 @@ app.post("/urls", (req, res) => {
     fav: false, 
     visitCount: 0, 
     uniqueVisits: [], 
-    visitTimes: [] 
-  };    //persists the url data
-  res.redirect(`/urls/${shortURL}`); //redirects to the new page
+    visitTimes: [],
+    dateCreated: creationDate 
+  };    
+  res.redirect(`/urls/${shortURL}`); //redirects to the new url page
 });
 
-//handle favourite button being pressed
-//save fav cookie at url entry 
+//BONUS FEAUTRE!!!! Favourite button - toggles a t/f value on the urlDB. Click again to untoggle.
+//Favourites are saved between sessions.
 app.put("/:shortURL/fav", (req, res) => {
   const userIDCookie = req.session.user_id;
   if (!users[userIDCookie]) {
@@ -110,7 +118,7 @@ app.put("/:shortURL/fav", (req, res) => {
     res.redirect("/urls");
   }
 });
-
+//Handle unfavouritizing 
 app.put("/:shortURL/unfav", (req, res) => {
   const userIDCookie = req.session.user_id;
   if (userIDCookie !== users[userIDCookie].id) {
@@ -123,21 +131,18 @@ app.put("/:shortURL/unfav", (req, res) => {
   }
 });
 
-//redirect requests to /u/:shortUrl to the respective longUrl
+//Redirect requests fromvalid short URL to the respective longUrl, increment the url's counter and edit its visitor data arrays for rendering 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const userIDCookie = req.session.user_id
-  //count++ time this request is made
   urlDatabase[shortURL]['visitCount'] ++;
   //if it's a unique visit, log it
   if (!urlDatabase[shortURL]['uniqueVisits'].includes(userIDCookie)) {
     urlDatabase[shortURL]['uniqueVisits'].push(userIDCookie);
-    const visitDate = new Date();
-    var visitTime = visitDate.getHours() + ":" + visitDate.getMinutes() + " UTC on " + visitDate.getDate() + "/" + visitDate.getMonth() + "/" + visitDate.getFullYear()
+    const visitTime = date.format(new Date(), 'MMM D YYYY hh:mm [UTC]', true)
     urlDatabase[shortURL]['visitTimes'].push(visitTime);
-    console.log(urlDatabase[shortURL])
   };
-  //if we have this entry in the database
+  //Prepend "http://" to urls that do now include www. (handles a error caused by URLs without www at the start)
   if (urlDatabase[shortURL]) {
     const formattedLongURL = urlDatabase[shortURL]['longURL'].substring(0, 4) !== 'http' ? `http://${urlDatabase[shortURL]['longURL']}` : urlDatabase[shortURL]['longURL'];
     res.redirect(formattedLongURL);
@@ -146,7 +151,7 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
-//add a route to remove a url resource
+//Delete a url entry from the database. Users can only delete their own entries.
 app.delete("/urls/:shortURL", (req, res) => {
   const userIDCookie = req.session.user_id;
   if (!users[userIDCookie]) {
@@ -158,7 +163,7 @@ app.delete("/urls/:shortURL", (req, res) => {
   }
 });
 
-//add a route for creating a new tiny url
+//Get the new urls page - verify that the user is logged in before they can access and redirect them if not
 app.get("/urls/new", (req, res) => {
   const userID = req.session.user_id;
   if (!users[userID]) {
@@ -169,26 +174,41 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
-//add a route handler for /urls__show
+//Route to show the short url feature page where users can edit the url and view analytics
+//only owners can view this page. Logged in non-owners are sent to a non-ownership page
 app.get("/urls/:shortURL", (req, res) => {
-  const userID = req.session.user_id; //time.now()
-  if (!users[userID]) {
+  const currentUserID = req.session.user_id; 
+  const shortURL = req.params.shortURL;
+  //If the current user isn't registered/logged in
+  if (!users[currentUserID]) {
     res.redirect("/login");
+  //if the database doesn't contain the short URL
+  } else if (!urlDatabase[shortURL]) {
+    res.redirect("/urls")
+    //if they are not the registered owner on the url database
+  } else if (urlDatabase[shortURL]['userID'] !== currentUserID) {
+    res.redirect("/urls/noshow");
   } else {
-    const shortURL = req.params.shortURL;
+    //all good, they can view
     const templateVars = { 
       shortURL, 
       longURL: urlDatabase[shortURL].longURL,
-      user: users[userID],
+      user: users[currentUserID],
       visitCount: urlDatabase[shortURL].visitCount,
       uniqueVisits: urlDatabase[shortURL].uniqueVisits, 
-      visitTimes: urlDatabase[shortURL].visitTimes
+      visitTimes: urlDatabase[shortURL].visitTimes,
+      dateCreated: urlDatabase[shortURL].dateCreated
     };
     res.render("urls_show", templateVars);
   }
 });
 
-//add a handler for updating a long URL
+//Unique page to send users who are logged in but try to access the views page of a url they don't own
+app.get("/urls/noshow", (req, res) => {
+  res.render("urls_noshow", users);
+});
+
+//Route handler for updating the long URL associated with a specific short URL
 app.post("/urls/:shortURL", (req, res) => {
   const updatedLongURL = req.body.updatedLongURL;
   const shortURL = req.params.shortURL;
@@ -196,40 +216,50 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
+//Route handler for logging out
 app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
 
+
+//Route for getting the register page. 
 app.get("/register", (req, res) => {
   res.render("register", users);
 });
 
-//put is wrong here
-app.put("/register", (req, res) => {
+//Post request to handle new user registeration and various errors for invalid input: email/password undefined, email already in use.
+//Redirect back to urls index page if registration is successful
+app.post("/register", (req, res) => {
   let id = generateRandomString();
   let email = req.body.email;
   const password = req.body.password;
   const hashedPassword = bcrypt.hashSync(password, 10);
-  //res.send error 400 if password empty or email empty
-  //email lookup finds email in userobj
   if (!email || !password) {
     return res.status(400).send('Email or password cannot be empty!');
   } else if (emailLookup(email, users)) {
     return res.status(400).send('Sorry, that email/password combination is invalid!');
   } else {
-    users[id] = {id, email, hashedPassword};
+    users[id] = {
+      id, 
+      email, 
+      hashedPassword
+    };
     req.session.user_id = id;
     res.redirect("/urls");
   }
 });
 
+
+//Get user log in page
 app.get("/login", (req, res) => {
   const templateVars = {user: null};
   res.render("login", templateVars);
 
 });
 
+//Handle log in attempt. Compare hashed password to records for authentication. 
+//Look up user hashed pass with email lookupfunction, return error message if credentials don't match.
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
--- helpers.js
+++ helpers.js
@@ -8,4 +8,20 @@ const emailLookup = (emailAddress, database) => {
   return undefined;
 };
 
-module.exports = { emailLookup };
+const generateRandomString = () => {
+  //loop from 1-6
+  //generate a random number between (48 - 90 excluding 58 - 64), get char code from this number
+  let retString = "";
+  while (retString.length < 6) {
+    let raNum = Math.ceil(Math.random() * (90 - 49) + 48);
+    if (raNum >= 58 && raNum <= 64) {
+      continue;
+    } else {
+      let addChar = String.fromCharCode(raNum);
+      retString += addChar;
+    }
+  }
+  return retString.toLowerCase();
+};
+
+module.exports = { emailLookup, generateRandomString };
